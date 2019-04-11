91a5d9db3527c80259002447fa67274a5145704e
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-11 16:43:16 +0200

    Update Ficha.java

--- Codigo/src/Dominio/fichas/Ficha.java
+++ Codigo/src/Dominio/fichas/Ficha.java
@@ -2,27 +2,29 @@ package Dominio.fichas;
 
 import java.util.*;
 import ClasesExtra.Coordenada;
-import Dominio.Problema;
+import Dominio.Tablero;
 /**
  *
  * @author Àlex
  */
 public abstract class Ficha {
     protected Coordenada posicion;
-    protected boolean color; //white = true, black = false
+    protected Boolean color; //white = true, black = false
+    protected Character c; //white = true, black = false
     
     public Ficha() {}
     
-    public Ficha(boolean color,Coordenada posicion) {
+    public Ficha(Boolean color,Coordenada posicion, char cfen) {
         this.color = color;
         this.posicion = posicion;
+        this.c = cfen;
     }
     
     public Coordenada getPosicion()  {
         return posicion;
     }
     
-    public boolean getColor() {
+    public Boolean getColor() {
         return color;
     }
     
@@ -30,9 +32,11 @@ public abstract class Ficha {
         color = c;
     }
     
+    
+    
     public void setPosicion(Coordenada p) {
         posicion = p;
     }
     
-    public abstract ArrayList<Coordenada> posiblesMovimientos(Problema p);  
+    public abstract ArrayList<Coordenada> posiblesMovimientos(Tablero p);  
 }
