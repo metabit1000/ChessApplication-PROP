ebdc94f62eab2e249d81436e551d53b18b2e217b
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-11 16:43:54 +0200

    Add files via upload

---
+++ Codigo/src/Dominio/ConjuntoProblemas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dominio;
+import java.util.*;
+/**
+ *
+ * @author joan.manuel.ramos
+ */
+public class ConjuntoProblemas {
+    
+
+    public static void main (String [] args) { 
+        ArrayList<String> Problemas = new ArrayList<>();
+        String fen = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w";
+        Problemas.add(fen);
+        
+    }
+}
