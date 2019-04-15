1678ad7b55ac0a3a3d047923c4cae10be4e5f364
jordi2326 <44190613+jordi2326@users.noreply.github.com>
2019-04-15 10:17:44 +0200

    Add files via upload

---
+++ Codigo/src/Dominio/DriverJugador.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dominio;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jordi
+ */
+public class DriverJugador {
+     public static void main (String [] args) throws IOException {
+        int estado = 0;
+        Jugador  Prueba;
+        int fin = 1000; //por poner algo...
+        Scanner sc = new Scanner(System.in);
+        while (estado != fin) {
+            System.out.println("Menú:");
+            System.out.println("1. Constructor");
+            System.out.println("Introduzca un número: ");
+                    
+            estado = sc.nextInt();
+            switch (estado) {
+                case 1:
+                  
+                    System.out.println("Ha elegido: Constructor");
+                  
+                   
+                      Prueba = new Jugador (); 
+                    System.out.println("Jugador creado correctamente ");
+                    break;
+    
+    
+    
+    
+            }
+
+ 
+        }
+     }
+}
---
+++ Codigo/src/Dominio/Jugador.java
@@ -0,0 +1,20 @@
+package Dominio;
+import java.util.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jordi
+ */
+public class Jugador {
+    
+    public Jugador() {}
+    
+    
+   
+     
+}
