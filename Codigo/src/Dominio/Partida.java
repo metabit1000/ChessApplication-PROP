0e61b6ca6d9d143043c0d6baca38928c1cd1b38d
jordi2326 <44190613+jordi2326@users.noreply.github.com>
2019-04-16 16:19:43 +0200

    Update Partida.java

--- Codigo/src/Dominio/Partida.java
+++ Codigo/src/Dominio/Partida.java
@@ -2,6 +2,7 @@ package Dominio;
 
 import ClasesExtra.Coordenada;
 import Dominio.fichas.Ficha;
+import java.util.ArrayList;
 
 /**
  *
@@ -9,12 +10,15 @@ import Dominio.fichas.Ficha;
  */
 public class Partida {
     private Problema p = new Problema();
-    //Jugador1
-    //Jugador2
-    //tiempo
-    //turno
+    private Jugador j1 = new Jugador();
+    private Jugador j2 = new Jugador();
+    private double time ;
+    private boolean turno  ;
     
-    public Partida() {}
+    public Partida() {
+        turno = false ; 
+    }
+    
     
     public void moveFicha(String s1, String s2) {
         //dadas dos posiciones, mueve la ficha de coord c1 a c2 siempre y cuando c2 se pueda acceder,
@@ -24,10 +28,28 @@ public class Partida {
         c1.stringToCoord(s1);
         Coordenada c2 = new Coordenada();
         c2.stringToCoord(s2);
-        
         Ficha f1 = p.getFicha(c1);
-        p.removeFicha(c1);
-        //}
-       // else System.out.println("Error");
+        if (f1 != null) {
+            ArrayList<Coordenada> pM = new ArrayList<>(); 
+            pM = f1.posiblesMovimientos(p,c1);
+            Boolean find = false;
+            for (int i = 0; i < pM.size() && !find; i++) {
+                Coordenada x = pM.get(i);
+                if (x.getX() == c2.getX() && x.getY() == c2.getY()) find = true;
+            }
+            if (find) {
+                p.setFicha(c2, f1);
+                removeFicha(c1);
+            }
+            else System.out.println("La coordenada de destino no es correcta.");
+        }
+        else System.out.println("En la coordenada de origen no hay ficha.");
     }
+    
+    public void removeFicha(Coordenada c) {
+        int x = c.getX();
+        int y = c.getY();
+        p.board[x][y] = null;
+    }
+  
 }
