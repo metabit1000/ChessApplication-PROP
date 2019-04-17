1db8dcda8fdf7b519862012e363912fdeee9be3f
metabit1000 <48792707+metabit1000@users.noreply.github.com>
2019-04-17 21:52:34 +0200

    Update Maquina.java

--- Codigo/src/Dominio/Maquina.java
+++ Codigo/src/Dominio/Maquina.java
@@ -8,8 +8,8 @@ import Dominio.Problema;
  * @author Àlex
  */
 public class Maquina extends Jugador {
-    int dificultad; //1 facil, 2 dificil
-    Minimax minimax1;
+    int dificultad = 1; //1 facil, 2 dificil
+    Minimax minimax1 = new Minimax(3,color);
     //MinimaxAlphaBeta minimax2;
     
     public Maquina(Boolean color,int dificultad) {
@@ -17,9 +17,9 @@ public class Maquina extends Jugador {
         this.dificultad = dificultad;
     }
     
-    public Coordenada getNextMove(Problema p) {
+    public Coordenada getNextMove(Problema p, Coordenada c) {
         Coordenada move = new Coordenada();
-        if(dificultad == 1) move = minimax1.decisionMinimax(p);
+        if(dificultad == 1) move = minimax1.decisionMinimax(p,c);
         //else if (dificultad == 2) minimax2.decisionMinimax(p);
         return move;
     }
