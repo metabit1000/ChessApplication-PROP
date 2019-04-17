945068ee0805959a4e407ffacd056c24a514b076
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-17 12:21:33 +0200

    Update Usuario.java

--- Codigo/src/Dominio/Usuario.java
+++ Codigo/src/Dominio/Usuario.java
@@ -17,8 +17,6 @@ public class Usuario extends Jugador {
     
     private String nom;
     private String pass;
-    private Boolean log;
-    private ArrayList<String> ProblemasSuperados;
     private ArrayList<String> ProblemasCreados;
     
     //Constructoras
@@ -27,10 +25,9 @@ public class Usuario extends Jugador {
     
     public Usuario(String nombre, String password, Boolean login) {
         //Comprobar que el nombre de usuario existe???????????????????????????
-        if (!correctPass(password)) System.out.println("La contraseña necesita como mínimo 6 carácteres y tener como mínimo una letra minúscula, una mayúscula y un número.");
+        //if (!correctPass(password)) System.out.println("La contraseña necesita como mínimo 6 carácteres y tener como mínimo una letra minúscula, una mayúscula y un número.");
         nom = nombre;
         pass = password;
-        log = login;
         //Añadir new ArrayLists de ProblemasSuperados y ProblemasCreados cuando tengamos la clase problemas
     }
     
@@ -40,35 +37,29 @@ public class Usuario extends Jugador {
     public String getPassword() {
         return this.pass;
     }
-    public Boolean isLog() {  
-        return this.log;
-    }      
     public void setPassword(String password) {
-        if (!correctPass(pass)) System.out.println("La contraseña necesita como mínimo 6 carácteres y tener como mínimo una letra minúscula, una mayúscula y un número.");
+        //if (!correctPass(pass)) System.out.println("La contraseña necesita como mínimo 6 carácteres y tener como mínimo una letra minúscula, una mayúscula y un número.");
         pass = password;
     }
-    public void setLog(Boolean login) {
-        log = login;
-    }
     
-    public Boolean correctPass(String pass) {//esta clase va aqui o en ConjuntoUsuarios??????????????????????
-        boolean n = false;
-        boolean m = false;
-        boolean M = false;
-        for (int j = 0; j < pass.length(); ++j) {
-            String mayu ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            for(int i = 0; i < mayu.length(); ++i) {
-                if (mayu.charAt(i) == pass.charAt(j)) M = true; // Es una letra mayuscula
-            }
-            String minu = "abcdefghijklmnopqrstuvwxyz";
-            for(int i = 0; i < minu.length(); ++i) {
-                if (minu.charAt(i) == pass.charAt(j)) m = true; // Es una letra minúscula
-            }
-            String num = "0123456789";
-            for(int i = 0; i < num.length(); ++i)
-                if (num.charAt(i) == pass.charAt(j)) n = true; // Es un número
-            }
-        return pass.length() > 5 && n && m && M;
-    }
+//    public Boolean correctPass(String pass) {//esta clase va aqui o en ConjuntoUsuarios??????????????????????
+//        boolean n = false;
+//        boolean m = false;
+//        boolean M = false;
+//        for (int j = 0; j < pass.length(); ++j) {
+//            String mayu ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//            for(int i = 0; i < mayu.length(); ++i) {
+//                if (mayu.charAt(i) == pass.charAt(j)) M = true; // Es una letra mayuscula
+//            }
+//            String minu = "abcdefghijklmnopqrstuvwxyz";
+//            for(int i = 0; i < minu.length(); ++i) {
+//                if (minu.charAt(i) == pass.charAt(j)) m = true; // Es una letra minúscula
+//            }
+//            String num = "0123456789";
+//            for(int i = 0; i < num.length(); ++i)
+//                if (num.charAt(i) == pass.charAt(j)) n = true; // Es un número
+//            }
+//        return pass.length() > 5 && n && m && M;
+//    }
     
 }
