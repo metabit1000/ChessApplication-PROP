7f10bb1987828230e3d80f4cdc69dd754754189a
jordi2326 <44190613+jordi2326@users.noreply.github.com>
2019-04-17 17:15:11 +0200

    Update Partida.java

--- Codigo/src/Dominio/Partida.java
+++ Codigo/src/Dominio/Partida.java
@@ -10,15 +10,15 @@ import java.util.ArrayList;
  */
 public class Partida {
     private Problema p = new Problema();
-    private Jugador j1 = new Jugador();
-    private Jugador j2 = new Jugador();
     private double time ;
     private boolean turno  ;
     
     public Partida() {
         turno = false ; 
     }
-    
+     public void setEstado(){
+         turno =!turno ; 
+     }
     
     public void moveFicha(String s1, String s2) {
         //dadas dos posiciones, mueve la ficha de coord c1 a c2 siempre y cuando c2 se pueda acceder,
@@ -29,6 +29,7 @@ public class Partida {
         Coordenada c2 = new Coordenada();
         c2.stringToCoord(s2);
         Ficha f1 = p.getFicha(c1);
+         boolean color = f1.getColor();
         if (f1 != null) {
             ArrayList<Coordenada> pM = new ArrayList<>(); 
             pM = f1.posiblesMovimientos(p,c1);
@@ -40,7 +41,10 @@ public class Partida {
             if (find) {
                 p.setFicha(c2, f1);
                 removeFicha(c1);
+                         boolean mat = mate(color) ;
+               if (mat) System.out.println("mate");
             }
+            
             else System.out.println("La coordenada de destino no es correcta.");
         }
         else System.out.println("En la coordenada de origen no hay ficha.");
@@ -49,7 +53,35 @@ public class Partida {
     public void removeFicha(Coordenada c) {
         int x = c.getX();
         int y = c.getY();
-        p.board[x][y] = null;
+        p.putnull(x,y);
     }
-  
+  public Boolean mate (Boolean color ){
+    Boolean  mate = false;
+    Ficha f1;
+        for (int i = 0; i < 8 ; i++) {
+              for (int j = 0; j < 8; j++) {
+                  Coordenada z = new Coordenada();
+                  z.setX(i);
+                  z.setY(j);
+                   if (p.getFicha(z)!= null && color == p.color(i, j)) {
+                       f1 = p.getFicha(z);
+                       ArrayList<Coordenada> pM1 = new ArrayList<>(); 
+                       pM1 = f1.posiblesMovimientos(p,z);
+                        for (int w = 0; w < pM1.size() ; w++) {
+                              Coordenada x = pM1.get(w);
+                              if (p.getFicha(x) != null ){
+                                  char f = p.getFicha(x).getID();
+                                  if (f == 'k' &&  color ) mate = true ; 
+                                  if (f == 'K' && !color) mate =true;
+                              }
+                             
+        }         
+      }
+     
+   }
+
+  }
+                return mate ; 
+
+}
 }
