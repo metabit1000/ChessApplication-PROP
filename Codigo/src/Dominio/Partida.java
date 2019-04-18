dac4aab314825e6040105f920da92371a05bd149
jordi2326 <44190613+jordi2326@users.noreply.github.com>
2019-04-18 12:50:56 +0200

    Update Partida.java

--- Codigo/src/Dominio/Partida.java
+++ Codigo/src/Dominio/Partida.java
@@ -6,20 +6,50 @@ import java.util.ArrayList;
 
 /**
  *
- * @author 
+ * @author Jordi
  */
 public class Partida {
-    private Problema p;
+    private Problema p = new Problema();
     private double time ;
     private boolean turno  ;
     
     public Partida() {
         turno = false ; 
     }
+    
+    
      public void setEstado(){
          turno =!turno ; 
      }
     
+//    public void moveFicha(String s1, String s2) {
+//        //dadas dos posiciones, mueve la ficha de coord c1 a c2 siempre y cuando c2 se pueda acceder,
+//        //no haya una ficha de igual color a la que movemos y este dentro del tablero. Si hay una ficha rival 
+//        //en c2, nos la comemos
+//        Coordenada c1 = new Coordenada();
+//        c1.stringToCoord(s1);
+//        Coordenada c2 = new Coordenada();
+//        c2.stringToCoord(s2);
+//        Ficha f1 = p.getFicha(c1);
+//        boolean color = f1.getColor();
+//        if (f1 != null) {
+//            ArrayList<Coordenada> pM = new ArrayList<>(); 
+//            pM = f1.posiblesMovimientos(p,c1);
+//            Boolean find = false;
+//            for (int i = 0; i < pM.size() && !find; i++) {
+//                Coordenada x = pM.get(i);
+//                if (x.getX() == c2.getX() && x.getY() == c2.getY()) find = true;
+//            }
+//            if (find) {
+//                p.setFicha(c2, f1);
+//                removeFicha(c1);
+//                if (p.checkmate(p.getFicha(c2).getColor())) System.out.println("hey que es jaquemate");
+//            }
+//            
+//            else System.out.println("La coordenada de destino no es correcta.");
+//        }
+//        else System.out.println("En la coordenada de origen no hay ficha.");
+//    }
     public void moveFicha(String s1, String s2) {
         //dadas dos posiciones, mueve la ficha de coord c1 a c2 siempre y cuando c2 se pueda acceder,
         //no haya una ficha de igual color a la que movemos y este dentro del tablero. Si hay una ficha rival 
@@ -29,7 +59,6 @@ public class Partida {
         Coordenada c2 = new Coordenada();
         c2.stringToCoord(s2);
         Ficha f1 = p.getFicha(c1);
-         boolean color = f1.getColor();
         if (f1 != null) {
             ArrayList<Coordenada> pM = new ArrayList<>(); 
             pM = f1.posiblesMovimientos(p,c1);
@@ -39,17 +68,23 @@ public class Partida {
                 if (x.getX() == c2.getX() && x.getY() == c2.getY()) find = true;
             }
             if (find) {
+                
                 p.setFicha(c2, f1);
+                p.setlmC1(c1);
+                p.setlmC2(c2);
                 removeFicha(c1);
-                boolean mat = mate(color) ;
-               if (mat) System.out.println("mate");
+                if (mate(f1.getColor())){
+                    System.out.println("es mate");
+                    if(checkmate(f1.getColor())) System.out.println("es Jaquemate");
+
             }
-            
+                 p.setFicha(c2, f1);
+
+            }
             else System.out.println("La coordenada de destino no es correcta.");
         }
         else System.out.println("En la coordenada de origen no hay ficha.");
     }
-    
     public void removeFicha(Coordenada c) {
         int x = c.getX();
         int y = c.getY();
@@ -80,6 +115,29 @@ public class Partida {
             }
         }
         return mate ; 
+    }
+    
+    public Boolean checkmate(Boolean color) {
+        Boolean checkmate = true;
+        for (int i = 0; i < 8 ; i++) {
+            for (int j = 0; j < 8 ; j++) {
+                Coordenada z = new Coordenada();
+                z.setX(i);
+                z.setY(j);
+                if (p.getFicha(z)!= null && color != p.getFicha(z).getColor()) {
+                    Ficha f1 = p.getFicha(z);
+                    ArrayList<Coordenada> pM1 = new ArrayList<>(); 
+                    pM1 = f1.posiblesMovimientos(p,z);
+                    for (int w = 0; w < pM1.size() ; w++) {
+                        p.moveFicha(z.coordToString(),pM1.get(w).coordToString());
+                        boolean color1 = mate(color);
+                        if (!color1) checkmate=false  ; 
+                        p.undoMove();
+                    }
+                }
+            }
+        }
+        return checkmate;
     }
 }
 
