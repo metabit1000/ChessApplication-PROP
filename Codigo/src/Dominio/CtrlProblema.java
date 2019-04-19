fd31e27966582dea7ed543d9ed8abf5e08e165aa
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-19 16:18:11 +0200

    Add files via upload

---
+++ Codigo/src/Dominio/CtrlProblema.java
@@ -0,0 +1,18 @@
+package Dominio;
+import java.util.*;
+
+/**
+ *
+ * @author jota
+ */
+public class CtrlProblema {
+    private Map<String, String> Problems = new HashMap<String, String>();
+    
+    public CtrlProblema() {
+            Problems.put("Problema 1. Mate en 2.", "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w");
+            Problems.put("Problema 2. Mate en 3.", "3K4/8/8/p2k4/pp1B4/N5N1/P2Q4/8 w");
+            Problems.put("Problema 2. Mate en 3.", "1rb4r/p1q2pnk/4pBpp/2p1P3/2P2QP1/3BR3/P4P1P/3R2K1 w");
+    }
+    
+    
+}
