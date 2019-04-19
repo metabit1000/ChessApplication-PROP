d92627cc4c151a02631e06d36467f97b47d60622
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-19 15:50:44 +0200

    Update Usuario.java

--- Codigo/src/Dominio/Usuario.java
+++ Codigo/src/Dominio/Usuario.java
@@ -1,11 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Dominio;
-import java.util.*;
-
 
 /**
  *
@@ -13,23 +6,19 @@ import java.util.*;
  */
 public class Usuario extends Jugador {
     
-    //Atributos
-    
     private String nom;
     private String pass;
-    private ArrayList<String> ProblemasCreados;
-    
-    //Constructoras
-    
+
     public Usuario() {}
     
-    public Usuario(String nombre, String password, Boolean login) {
-        //Comprobar que el nombre de usuario existe???????????????????????????
-        //if (!correctPass(password)) System.out.println("La contraseña necesita como mínimo 6 carácteres y tener como mínimo una letra minúscula, una mayúscula y un número.");
+    public Usuario(String nombre, String password) {
         nom = nombre;
         pass = password;
-        //Añadir new ArrayLists de ProblemasSuperados y ProblemasCreados cuando tengamos la clase problemas
     }
+    
+    public void setNombre(String nombre) {  
+        nom = nombre;
+    } 
     
     public String getNombre() {  
         return this.nom;
@@ -38,28 +27,6 @@ public class Usuario extends Jugador {
         return this.pass;
     }
     public void setPassword(String password) {
-        //if (!correctPass(pass)) System.out.println("La contraseña necesita como mínimo 6 carácteres y tener como mínimo una letra minúscula, una mayúscula y un número.");
         pass = password;
     }
-    
-//    public Boolean correctPass(String pass) {//esta clase va aqui o en ConjuntoUsuarios??????????????????????
-//        boolean n = false;
-//        boolean m = false;
-//        boolean M = false;
-//        for (int j = 0; j < pass.length(); ++j) {
-//            String mayu ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//            for(int i = 0; i < mayu.length(); ++i) {
-//                if (mayu.charAt(i) == pass.charAt(j)) M = true; // Es una letra mayuscula
-//            }
-//            String minu = "abcdefghijklmnopqrstuvwxyz";
-//            for(int i = 0; i < minu.length(); ++i) {
-//                if (minu.charAt(i) == pass.charAt(j)) m = true; // Es una letra minúscula
-//            }
-//            String num = "0123456789";
-//            for(int i = 0; i < num.length(); ++i)
-//                if (num.charAt(i) == pass.charAt(j)) n = true; // Es un número
-//            }
-//        return pass.length() > 5 && n && m && M;
-//    }
-    
 }
