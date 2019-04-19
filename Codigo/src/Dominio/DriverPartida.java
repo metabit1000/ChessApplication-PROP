8e7c1bc00931200fcded2dbb2da6b2ba4924daf8
metabit1000 <48792707+metabit1000@users.noreply.github.com>
2019-04-19 21:57:14 +0200

    Update DriverPartida.java

--- Codigo/src/Dominio/DriverPartida.java
+++ Codigo/src/Dominio/DriverPartida.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  */
 public class DriverPartida {
     
-    public static void main (String [] args) throws Exception {
+    public static void main (String [] args) {
         int estado = 0;
         int fin = 1000; //por poner algo...
         Scanner sc = new Scanner(System.in);
@@ -18,13 +18,14 @@ public class DriverPartida {
    
         while (estado != fin) {
             System.out.println("Usuarios:");
-                        System.out.println("1. Tablero");
-            System.out.println("2. Mover ficha");
-            System.out.println("4. Exit");
+            System.out.println("1. Tablero");
+            System.out.println("2. Jugar");
+            System.out.println("3. Exit");
             
             //faltaria logout y hacer booleano en usuarios que diga si un usuario está logeado
             System.out.println("Introduzca un número: ");
             estado = sc.nextInt();
+            char color;
             switch (estado) {
                  case 1:
                     String fen = "1r6/8/2r5/8/3k4/8/P7/K7 b";
@@ -32,15 +33,39 @@ public class DriverPartida {
                     prueba2.printTablero();
                     break;
                 case 2:
-                    String c1;
-                    String c2;
-                    boolean b1;
-                    boolean b2 ;
-                    Coordenada c = new Coordenada();
-                    Integer contador = 1; 
-                    System.out.println("Ha elegido: Mover ficha");
-                     System.out.println("Jugador1 introduzca color :true->Blancas , false->Negras");
-                       b1=sc.nextBoolean();
+                    Partida partida = new Partida();
+                    Usuario us1 = new Usuario();
+                    Usuario us2 = new Usuario();
+                    System.out.println("Ha elegido: Jugar");
+                    System.out.println("Introduzca opción que desea jugar: ");
+                    System.out.println("1. JugadorVsJugador"); //faltaria loguearse
+                    System.out.println("2. JugadorVsMaquina1");
+                    System.out.println("3. JugadorVsMaquina2");
+                    System.out.println("3. Maquina1VsMaquina2");
+                    int opcion = sc.nextInt();
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Jugador1, ¿color?");
+                            color = sc.next().charAt(0);
+                            sc.nextLine();
+                            if (color == 'n'){
+                                us1 = new Usuario(false);
+                                us2 = new Usuario(true);
+                            }
+                            else if (color == 'b') {
+                                us1 = new Usuario(true);
+                                us2 = new Usuario(false);
+                            } 
+                            else {
+                                System.out.println("Error, vuelva a intentarlo");
+                                break;
+                            }
+                            partida = new Partida(us1,us2,prueba2);
+                            break;
+                    }
+                    
+                    
+                    b1=sc.nextBoolean();
                        sc.nextLine();
                        Jugador jugador1= new Jugador(b1);
                        Jugador jugador2= new Jugador(!b1);
@@ -58,8 +83,7 @@ public class DriverPartida {
                     System.out.println("Introduzca un Coordenada de destino: ");
                     c2 = sc.next();
                     sc.nextLine();
-                    prueba.setcosas(c1, c2);
-                    prueba.jugar(b1);
+                    prueba.jugar(b1,c1,c2);
                     if(prueba.checkmate(b1)) {
                         mate=true;
                     }                    
@@ -67,8 +91,8 @@ public class DriverPartida {
                        }
                     break;
                   
-                case 4: 
-                    fin = 4;
+                case 3: 
+                    fin = 3;
                     System.out.println("Gracias. Que tenga un buen día.");
                     break;
             }
