c10d4538662a23abc935c53f260b14f258447889
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-19 15:49:37 +0200

    Update CtrlUsuarios.java

--- Codigo/src/Dominio/CtrlUsuarios.java
+++ Codigo/src/Dominio/CtrlUsuarios.java
@@ -1,22 +1,34 @@
 package Dominio;
 import java.util.*;
-import Dominio.Usuario;
 
 /**
  *
  * @author joan
  */
 public class CtrlUsuarios {    
-    private Map<String,String> Users = new HashMap<>();
+    private final Map<String,String> Users = new HashMap<>();
     private String UserLogged;
     private String Guest;
     
     public CtrlUsuarios() {}
     
+    public String getUserLogged() {
+        if (UserLogged != null ) return UserLogged;
+        else return "No hay usuario logeado";
+    }
+    
+    public String getGuest() {
+        if (Guest != null ) return Guest;
+        else return "No hay guest disponible";
+    }
+    
     public void registrarUsuario(String nom, String pass) {
         if (!existUser(nom)) {
-            Users.put(nom, pass);
-            new Usuario(nom,pass,false);
+            if (correctPass(pass)) {
+                Users.put(nom, pass);
+                System.out.println("Usuario registrado correctamente.");
+            }
+            else System.out.println("La contraseña necesita como mínimo 6 carácteres y tener como mínimo una letra minúscula, una mayúscula y un número.");
         }
         else System.out.println("El usuario con nombre " + nom + " ya existe. Prueba con otro.");
     }
@@ -24,7 +36,10 @@ public class CtrlUsuarios {
     public void loginUsuario(String nom, String pass) {
         if (!Users.containsKey(nom)) System.out.println("El nombre de usuario es incorrecto");
         else {
-            if (Users.get(nom).equals(pass)) UserLogged = nom;
+            if (Users.get(nom).equals(pass)) {
+                UserLogged = nom;
+                System.out.println("Sesión iniciada satisfactoriamente");
+            }
             else System.out.println("La contraseña es incorrecta.");
         }
     }
@@ -45,37 +60,34 @@ public class CtrlUsuarios {
         Guest = null;
     }
     
-    
-//    public void modificarPassword(String nom, String newpassword) {
-//        Usuario u = new Usuario();
-//        u = consultarUsuario(nom);
-//        if (u.correctPass(newpassword)) u.setPassword(newpassword);
-//    }
-    
-//    public Usuario consultarUsuario(String nom) {
-//        Usuario u = new Usuario();
-//        Boolean find = false;
-//        for (int i = 0; i < Users.size() && !find; i++) {
-//            u = Users.get(i);
-//            String n = u.getNombre();
-//            if (nom.equals(n)) {
-//                find = true;
-//            }
-//        }
-//        return u;
-//    }
+    public Boolean correctPass(String pass) {
+        boolean n = false;
+        boolean m = false;
+        boolean M = false;
+        for (int j = 0; j < pass.length(); ++j) {
+            String mayu ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for(int i = 0; i < mayu.length(); ++i) {
+                if (mayu.charAt(i) == pass.charAt(j)) M = true; // Es una letra mayuscula
+            }
+            String minu = "abcdefghijklmnopqrstuvwxyz";
+            for(int i = 0; i < minu.length(); ++i) {
+                if (minu.charAt(i) == pass.charAt(j)) m = true; // Es una letra minúscula
+            }
+            String num = "0123456789";
+            for(int i = 0; i < num.length(); ++i)
+                if (num.charAt(i) == pass.charAt(j)) n = true; // Es un número
+            }
+        return pass.length() > 5 && n && m && M;
+    }
     
     public Boolean existUser(String nom) {
         return Users.containsKey(nom);
     }
     
-//    public void printUsuarios() {
-//        for (int i = 0; i < Users.size(); i++) {
-//            Usuario u = Users.get(i);
-//            String n = u.getNombre();
-//            String p = u.getPassword();
-//            System.out.println(n);
-//            System.out.println(p);
-//        }
-//    }
+    public void printUsuarios() {
+        for(String key : Users.keySet()) {
+            System.out.println(key);
+        }
+    }
 }
+
