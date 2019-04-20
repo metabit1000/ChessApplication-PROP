7c4a1d7f4d1cd10093e6cbab219d5b348509e623
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-20 18:25:24 +0200

    Update DriverPartida.java

--- Codigo/src/Dominio/DriverPartida.java
+++ Codigo/src/Dominio/DriverPartida.java
@@ -14,12 +14,11 @@ public class DriverPartida {
         int fin = 1000; //por poner algo...
         Scanner sc = new Scanner(System.in);
         Problema prueba = new Problema(2);
-   
+        CtrlProblema cp = new CtrlProblema();
         while (estado != fin) {
-            System.out.println("Usuarios:");
-            System.out.println("1. Tablero");
-            System.out.println("2. Jugar");
-            System.out.println("3. Exit");
+            System.out.println("Partida:");
+            System.out.println("1. Jugar");
+            System.out.println("2. Exit");
             
             //faltaria logout y hacer booleano en usuarios que diga si un usuario está logeado
             System.out.println("Introduzca un número: ");
@@ -27,24 +26,28 @@ public class DriverPartida {
             char color;
             switch (estado) {
                  case 1:
-                    String fen = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w";
-                    prueba.fenToMatrix(fen);
-                    prueba.printTablero();
-                    break;
-                case 2:
                     Partida partida = new Partida();
                     Usuario us1 = new Usuario();
                     Usuario us2 = new Usuario();
                     System.out.println("Ha elegido: Jugar");
+                    System.out.println("Lista de los problemas registrados:");
+                    cp.printProblemas();
+                    System.out.println("Introduzca el índice del problema que desea jugar: ");
+                    int problema = sc.nextInt();
+                    sc.nextLine();
+                    String f = cp.seleccionProblema(problema);
+                    prueba.fenToMatrix(f);
+                    prueba.printTablero();
                     System.out.println("Introduzca opción que desea jugar: ");
                     System.out.println("1. JugadorVsJugador"); //faltaria loguearse
                     System.out.println("2. JugadorVsMaquina1");
                     System.out.println("3. JugadorVsMaquina2");
                     System.out.println("4. Maquina1VsMaquina2");
                     int opcion = sc.nextInt();
+                    sc.nextLine();
                     switch (opcion) {
                         case 1:
-                            System.out.println("Jugador1, ¿color?");
+                            System.out.println("Jugador1, ¿color? (n/b)");
                             color = sc.next().charAt(0);
                             sc.nextLine();
                             if (color == 'n'){
@@ -60,13 +63,16 @@ public class DriverPartida {
                                 break;
                             }
                             partida = new Partida(us1,us2,prueba);
-                            System.out.println("Partida creada, jugador2 juega con; "+ us2.getcolor());
+                            String c;
+                            if (us2.getcolor()) c = "blancas.";
+                            else c = "negras";
+                            System.out.println("Partida creada, jugador2 juega con "+ c);
                             partida.playJugadores();
                             break;
                     }
                     break;
-                case 3: 
-                    fin = 3;
+                case 2:
+                    fin = 2;
                     System.out.println("Gracias. Que tenga un buen día.");
                     break;
             }
