f3e1cb56bc55cf8262969de33f2fa0ee505a5645
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-21 19:01:30 +0200

    Update CtrlProblema.java

--- Codigo/src/Dominio/CtrlProblema.java
+++ Codigo/src/Dominio/CtrlProblema.java
@@ -31,6 +31,8 @@ public class CtrlProblema {
             Problems.put("Problema 1. Mate en 2.", "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w");
             Problems.put("Problema 2. Mate en 3.", "3K4/8/8/p2k4/pp1B4/N5N1/P2Q4/8 w");
             Problems.put("Problema 3. Mate en 3.", "1rb4r/p1q2pnk/4pBpp/2p1P3/2P2QP1/3BR3/P4P1P/3R2K1 w");
+            Problems.put("Inicial", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w");
+            
     }
     
     public void addProblema(String id, String fen) {
