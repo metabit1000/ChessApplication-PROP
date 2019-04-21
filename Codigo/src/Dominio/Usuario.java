a89e48d1c046b9c6a4a9c513a2be391565031378
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-21 19:09:13 +0200

    Update Usuario.java

--- Codigo/src/Dominio/Usuario.java
+++ Codigo/src/Dominio/Usuario.java
@@ -1,5 +1,9 @@
 package Dominio;
 
+import ClasesExtra.Coordenada;
+import ClasesExtra.Pair;
+import java.util.Scanner;
+
 /**
  *
  * @author joan
@@ -8,7 +12,7 @@ public class Usuario extends Jugador {
     
     private String nom;
     private String pass;
-
+    
     public Usuario() {}
     
     public Usuario(boolean color) {
@@ -32,5 +36,21 @@ public class Usuario extends Jugador {
     }
     public void setPassword(String password) {
         pass = password;
+    }
+    
+    public Pair getNextMove(Problema p) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca coordenada origen, ex e4: ");
+        String c1 = sc.next();
+        sc.nextLine();
+        System.out.println("Introduzca coordenada final, ex e4: ");
+        String c2 = sc.next();
+        sc.nextLine();
+        Coordenada s1 = new Coordenada();
+        s1.stringToCoord(c1);
+        Coordenada s2 = new Coordenada();
+        s2.stringToCoord(c2);
+        Pair moves = new Pair(s1,s2);
+        return moves;
     }
 }
