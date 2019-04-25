a8ea20dd6e36a1cbb37ddcdf9387211ad8e00ae9
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-04-26 00:48:48 +0200

    Update CtrlUsuarios.java

--- Codigo/src/Dominio/CtrlUsuarios.java
+++ Codigo/src/Dominio/CtrlUsuarios.java
@@ -13,16 +13,20 @@ public class CtrlUsuarios {
     public CtrlUsuarios() {}
     
     public String getUserLogged() {
+        //devuelve el nombre del usuario que está logeado si lo hay.
         if (UserLogged != null ) return UserLogged;
         else return "No hay usuario logeado";
     }
     
     public String getGuest() {
+        //devuelve el nombre del usuario que ha iniciado sesión como invitado si lo hay.
         if (Guest != null ) return Guest;
         else return "No hay guest disponible";
     }
     
     public void registrarUsuario(String nom, String pass) {
+        //da de alta un usuario con nombre nom y contraseña pass. en el caso de que ya exista un usuario con nombre nom, no se dará de alta
+        //y en el caso de que la contraseña no cumpla las condiciones tampoco se dará de alta.
         if (!existUser(nom)) {
             if (correctPass(pass)) {
                 Users.put(nom, pass);
@@ -34,6 +38,8 @@ public class CtrlUsuarios {
     }
     
     public void loginUsuario(String nom, String pass) {
+        //el usuario con nombre nom y contraseñas pass hace login en el caso de que el nombre de usuario y contraseña coincidan con un usuario
+        //dado de alta previamente
         if (!Users.containsKey(nom)) System.out.println("El nombre de usuario es incorrecto");
         else {
             if (Users.get(nom).equals(pass)) {
@@ -45,6 +51,8 @@ public class CtrlUsuarios {
     }
     
     public void loginGuest(String nom, String pass) {
+       //el usuario con nombre nom y contraseñas pass inicia sesión como invitado en el caso de que el nombre de usuario y contraseña coincidan con un usuario
+        //dado de alta previamente
         if (!Users.containsKey(nom)) System.out.println("El nombre de usuario es incorrecto");
         else {
             if (Users.get(nom).equals(pass)) Guest = nom;
@@ -53,14 +61,18 @@ public class CtrlUsuarios {
     }
     
     public void logoutUsuario() {
+        //el usuario que habia logeado previamente pasa a no estarlo y por lo tanto el numbre del usuario loggeado pasa a ser null.
         UserLogged = null;
     }
     
     public void logoutGuest() {
+        //el usuario que habia logeado como invitado previamente pasa a no estarlo y por lo tanto el numbre del usuario loggeado pasa a ser null.
         Guest = null;
     }
     
     public Boolean correctPass(String pass) {
+        //devuelve true si la contraseña pass cumple los requisitos: minimo 6 caracteres, como mínimo una letra mayuscula, una minuscula y un numero
+        //de no cumplir uno de estos requisitos devolverá false.
         boolean n = false;
         boolean m = false;
         boolean M = false;
@@ -81,10 +93,12 @@ public class CtrlUsuarios {
     }
     
     public Boolean existUser(String nom) {
+        //devuelve true si el usuario con nombre nom existe en el map de usuarios. false en caso contrario
         return Users.containsKey(nom);
     }
     
     public void printUsuarios() {
+        //printea el nombre de todos los usuarios que se han dado de alta hasta el momento
         for(String key : Users.keySet()) {
             System.out.println(key);
         }
