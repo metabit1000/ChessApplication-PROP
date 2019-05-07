a98b7791fd32896229ce412267a8930f54a77658
metabit1000 <aslexag0@gmail.com>
2019-05-07 19:44:57 +0200

    Algunos cambios para Ctrldores

--- Codigo/src/Persistencia/CtrlDatosUsuarios.java
+++ Codigo/src/Persistencia/CtrlDatosUsuarios.java
@@ -1,16 +1,12 @@
 package Persistencia;
 
-import Dominio.Usuario;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 /**
