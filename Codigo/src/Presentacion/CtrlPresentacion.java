aa95f37172d8f5f583d02a8cb470b92ba587c514
jordi2326 <44190613+jordi2326@users.noreply.github.com>
2019-05-07 16:49:41 +0200

    Update CtrlPresentacion.java

--- Codigo/src/Presentacion/CtrlPresentacion.java
+++ Codigo/src/Presentacion/CtrlPresentacion.java
@@ -1,9 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Presentacion;
-
+import Dominio.CtrlUsuarios;
+import java.util.*;
 /**
  *
- * @author Àlex
+ * @author Jordi
  */
-public class CtrlPresentacion {
-    
+public class CtrlUsuariosPre {
+    CtrlUsuarios u = new CtrlUsuarios();
+
+  public Boolean UsuarioRegistrado(String nom ){
+     return  u.existUser(nom);
+      
+  }
+  
+  public Boolean  LogIn(String nom , String Password){
+      if(UsuarioRegistrado(nom) ) {
+          u.loginUsuario(nom, Password);
+          return true ; 
+      }
+      else return false ; 
+     
+  }
+  public Boolean Registro(String nom , String Password){
+      if(!UsuarioRegistrado(nom) ){
+          u.registrarUsuario(nom, Password);
+          return true ; 
+      }
+      else return false ; 
+  }
 }
