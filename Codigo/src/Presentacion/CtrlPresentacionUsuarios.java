8f5358c0ac183acfcde9381032ff2e1d73e8ef63
jordi2326 <44190613+jordi2326@users.noreply.github.com>
2019-05-10 17:31:29 +0200

    Add files via upload

--- Codigo/src/Presentacion/CtrlPresentacionUsuarios.java
+++ Codigo/src/Presentacion/CtrlPresentacionUsuarios.java
@@ -2,31 +2,49 @@ package Presentacion;
 import Dominio.CtrlUsuarios;
 import Dominio.Usuario;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Jordi
  */
 public class CtrlPresentacionUsuarios {
-    private CtrlUsuarios ctrlU = new CtrlUsuarios();
+    CtrlUsuarios ctrlU = new CtrlUsuarios();
 
-    public Boolean UsuarioRegistrado(Usuario u) throws IOException{
-        return ctrlU.existUser(u);
+    public Boolean UsuarioRegistrado(String nom,String password) {
+                Usuario u = new Usuario(false,nom,password);
+
+        try {
+            return ctrlU.existUser(u);
+        } catch (IOException ex) {
+            Logger.getLogger(CtrlPresentacionUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+        
     }
   
-    public Boolean LogIn(String nom,String password) throws IOException{
+    public Boolean LogIn(String nom,String password) {
         Usuario u = new Usuario(false,nom,password);
-        if(UsuarioRegistrado(u)) {
-            ctrlU.loginUsuario(nom, password);
+        if(UsuarioRegistrado(nom,password)) {
+            try {
+                ctrlU.loginUsuario(nom, password);
+            } catch (IOException ex) {
+                Logger.getLogger(CtrlPresentacionUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+            }
             return true; 
         }
         else return false; 
     }
     
-    public Boolean Registro(String nom,String password) throws IOException{
+    public Boolean Registro(String nom,String password) {
         Usuario u = new Usuario(false,nom,password);
-        if(!UsuarioRegistrado(u)){
-            ctrlU.registrarUsuario(nom, password);
+        if(!UsuarioRegistrado(nom,password)){
+            try {
+                ctrlU.registrarUsuario(nom, password);
+            } catch (IOException ex) {
+                Logger.getLogger(CtrlPresentacionUsuarios.class.getName()).log(Level.SEVERE, null, ex);
+            }
             return true; 
         }
         else return false; 
