6eb05afb0f8d1b9d81f1f5838c6d175cf01bb315
jordi2326 <44190613+jordi2326@users.noreply.github.com>
2019-05-22 19:38:05 +0200

    Update MaquinaHard.java

--- Codigo/src/Dominio/MaquinaHard.java
+++ Codigo/src/Dominio/MaquinaHard.java
@@ -13,7 +13,12 @@ public class MaquinaHard extends Maquina {
     MaquinaHard(Boolean color,int profundidad) {
         super(color,profundidad);
     }
-    
+     /**
+     * pre:Dado un problema queremos obtener un Pair de una posicion valida
+     * post:Devolvera un Pair correcto y con el mejor movimientos entre los posibles
+     * @param p
+     * @return 
+     */
     public Pair getNextMove(Problema p) {
         MinimaxAlphaBeta m = new MinimaxAlphaBeta();
         Pair move = new Pair();
