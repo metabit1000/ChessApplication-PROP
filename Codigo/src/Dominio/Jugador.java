6f8409ad4a87b3f3a89bf471c1cb69ec98e999aa
jordi2326 <44190613+jordi2326@users.noreply.github.com>
2019-05-22 19:31:34 +0200

    Update Jugador.java

--- Codigo/src/Dominio/Jugador.java
+++ Codigo/src/Dominio/Jugador.java
@@ -19,12 +19,20 @@ public class Jugador {
         this.color = color;
         this.nombre = nombre;
     }  
-    
+    /**
+ * pre:Dado un Jugador con un color queremos obtener su color
+ * post:devuleve un booleano el cual nos devuelve false si es negro y si es blanco es true 
+ * @return color
+ */    
     public boolean getColor(){
         //devuleve el color del jugador
         return color;
     }
-    
+    /**
+     * pre:Dado un jugador queremos obtener su nombre 
+     * post:Devolvemo el nombre del jugador
+     * @return nombre
+     */
     public String getNombre() {
         //devuelve el nombre del usuario
         return nombre;
