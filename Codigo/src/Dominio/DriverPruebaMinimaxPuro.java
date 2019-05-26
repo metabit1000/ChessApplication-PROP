7b1288230fb3b40a474544ef5dc6cd949d7d080e
jrefusta <47823944+jrefusta@users.noreply.github.com>
2019-05-26 18:04:59 +0200

    Add files via upload

--- Codigo/src/Dominio/DriverPruebaMinimaxPuro.java
+++ Codigo/src/Dominio/DriverPruebaMinimaxPuro.java
@@ -7,7 +7,8 @@ package Dominio;
  */
 public class DriverPruebaMinimaxPuro {
     public static void main(String[]args){
-        Problema p = new Problema(2,"6k1/p1p2rpp/1q6/2p5/4P3/PQ6/1P4PP/3R3K",1,null);
+        
+        Problema p = new Problema(40,"8/8/8/8/1n6/8/3QbkNK/4N3 w",2,null);
         if (p.validarProblema()) System.out.println("Funciona");
         else System.out.println("No hace una mierda");
     }
